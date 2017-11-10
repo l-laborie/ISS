2d91294f831de103b1a9fe64ba9ed006a91a062f
Ludovic Laborie <l.laborie@criteo.com>
2017-11-10 14:13:15 -0500

    First commit

---
+++ Element.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Element]
+(
+    [Id] INT NOT NULL PRIMARY KEY,
+    [ElementName] VARCHAR(50) NULL,
+    [ElementDefaultName] VARCHAR(50) NULL
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+ISS project
+===========
+
+**I**nformation for **S**tyle **S**torage.
+This database will store all diffrent style need to custom any SSRS reports
